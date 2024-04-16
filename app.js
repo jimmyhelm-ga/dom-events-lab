201396d9b4e11241a912c68b7076059c6e178002
Jimmy Helm <jimmyatthehelm@gmail.com>
2024-04-16 16:15:46 -0400

    updated version with ability to calculate

--- app.js
+++ app.js
@@ -1,61 +1,48 @@
+
 /*-------------------------------- Constants --------------------------------*/
-const numbers = document.querySelectorAll(".button")
-// 1. create a variable for .display
-const display = document.querySelector(".display")
+
+const numbers = document.querySelectorAll(".number");
+const display = document.querySelector(".display");
+const equalsButton = document.querySelector(".equals");
+const clearButton = document.querySelector(".button.C");
+const operators = document.querySelectorAll(".button:not(.number, .C, .equals)"); // On my first attempt, the calculate function wasn't working,
+//I asked chatGPT and it recommended using this :not method. I learned that what's happening is it's looking for all buttons that have
+//a button class, but doesn't want any with number, C, or equals, which allows it to find the buttons that perform calculations. 
+
 
 /*-------------------------------- Variables --------------------------------*/
-
-
-
-/*------------------------ Cached Element References ------------------------*/
-
-
-
+let expression = '';
 
 /*----------------------------- Event Listeners -----------------------------*/
 
-
 numbers.forEach(number => {
   number.addEventListener("click", (event) => {
-    const buttonText = event.target.innerText;
-
-
-    console.log(buttonText);
-
-    
-    addToDisplay(buttonText);
-
-    if (buttonText === "C") {
-      clearCalc();
-    } else if (buttonText === "=") {
-      calculateResult();
-    }
+    expression += event.target.innerText;
+    display.innerText = expression;
   });
 });
 
+operators.forEach(operator => {
+  operator.addEventListener("click", (event) => {
+    expression += event.target.innerText;
+    display.innerText = expression;
+  });
+});
 
-
-
-/*-------------------------------- Functions --------------------------------*/
-
-let clearCalc = () => {
-  display.innerHTML = "0";
-};
-
-let addToDisplay = (text) => {
-  if (display.innerHTML === "0" || display.innerHTML === "Error") {
-    display.innerHTML = text;
-  } else {
-    display.innerHTML += text;
+equalsButton.addEventListener("click", () => {
+  try {
+    const result = eval(expression);
+    display.innerText = result;
+    expression = result.toString();
+  } catch (error) {
+    display.innerText = 'Error';
+    expression = '';
   }
-};
+});
 
-let calculateResult = () => {
-  
-    display.innerHTML = eval(display.innerHTML);
-}
-
-
-
+clearButton.addEventListener("click", () => {
+  display.innerText = '';
+  expression = '';
+});
 
 
